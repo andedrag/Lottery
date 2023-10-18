9c7ccca63520acbac4ae4e07b31e41753b8f283a
andedrag <andedrag@gmail.com>
2023-10-19 00:19:52 +0200

    Validator class validateAndConvert() improved

--- src/LotteryWinnerCounter/Validator.java
+++ src/LotteryWinnerCounter/Validator.java
@@ -1,8 +1,6 @@
 package LotteryWinnerCounter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,35 +8,43 @@ public class Validator {
 
     private static final String NUMBER_PATTERN = "([1-9]|[1-8]\\d|90)";
     private static final Pattern LINE_PATTERN = Pattern.compile("(%1$s) (%1$s) (%1$s) (%1$s) (%1$s)".formatted(NUMBER_PATTERN));
+    private static final int DRAW_COUNT = 5;
 
     List<Set<Integer>> validateAndConvert(List<String> tickets) {
         List<Set<Integer>> validatedTickets = new ArrayList<>();
-        for (String ticket:tickets ) {
-
-
+        for (String ticket:tickets) {
+            ticket.trim();
+            Matcher m = LINE_PATTERN.matcher(ticket);
+            if (!m.matches()) {
+                //TODO log
+            } else {
+                Set<Integer> ticketAsSet = convert(ticket, m);
+               if ()
+            validatedTickets.add(convert(ticket));
+            }
         }
-        validate(tickets);
-        return convert(tickets);
+        //TODO log
+        return validatedTickets;
 
         /*
       TODO
-        - szóközök levágása elején, végén
-        - felesleges köztes szóközök eliminálása
-        - csak számként értelmezhető elemek legyenek benne
-        - pontosan 5 db számként értelmezhető eleme legyen
-        - regex a fentieket megoldja (check!)
+
         - minden szám csak egyszer szerepelhet egy sorban --> setekké alakítani a sorokat?
         - log
       */}
 
-    private Set<Integer> convert(String numbersInLinePatternForm) {
-        //TODO
-        return null;
+    private Set<Integer> convert(String numbersInLinePatternForm, Matcher m) {
+        Set<Integer> ticketAsSet = new HashSet<>();
+        //TODO convert algorythm
+        if (ticketAsSet.size() != DRAW_COUNT) {
+            //TODO log - ticket dropped due to repeated numbers within
+            return null;
+        }
+        return ticketAsSet;
     }
 
     private void validate(String numbers) {
-        numbers.trim();
-        Matcher m = LINE_PATTERN.matcher(numbers);
-        //TODO
+
+
             }
 }
